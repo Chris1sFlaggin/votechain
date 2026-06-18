fdddb4573ae362ebd5ccc2dc029d61b233d855e2
Michele_Molineri <90928038+MicheleMolineri@users.noreply.github.com>
2026-06-18 14:47:51 +0200

    address update

--- web/config.js
+++ web/config.js
@@ -2,7 +2,7 @@
 // SystemBootstrap deployato via Remix; Router/Factory/PollHub ricavati da addresses().
 const CONFIG = {
   chainId: 11155111, // Sepolia: il sito chiede a MetaMask di passare a questa rete
-  bootstrap: "0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8", // SystemBootstrap su Sepolia
+  bootstrap: "0xB4b583d0766650ef5e035373a67061E585d8F044", // SystemBootstrap su Sepolia
   router: "",
   factory: "",
   pollHub: "",
