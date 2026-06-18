106c14ea4771dc0ae7354ef8a5927c09406c9fbd
Chris1sFlaggin <marongiuloris@gmail.com>
2026-06-18 17:53:18 +0200

    Update config.js

--- web/config.js
+++ web/config.js
@@ -2,7 +2,7 @@
 // SystemBootstrap deployato via Remix; Router/Factory/PollHub ricavati da addresses().
 const CONFIG = {
   chainId: 11155111, // Sepolia: il sito chiede a MetaMask di passare a questa rete
-  bootstrap: "0x5463BD970F0962a64847d02E73AD51B217aA2DA4", // SystemBootstrap su Sepolia
+  bootstrap: "0x92A0b6Ad0c6bf05fd6447dFBb972Bd9F6133E2DD", // SystemBootstrap su Sepolia
   router: "",
   factory: "",
   pollHub: "",
