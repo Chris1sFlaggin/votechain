88d2142db874359b9e534b5f0d7158227fcae177
“Michele <michele.molineri@edu.unito.it>
2026-06-20 16:57:49 +0200

    build update

--- web/config.js
+++ web/config.js
@@ -2,7 +2,7 @@
 // SystemBootstrap deployato via Remix; Router/Factory/PollHub ricavati da addresses().
 const CONFIG = {
   chainId: 11155111, // Sepolia: il sito chiede a MetaMask di passare a questa rete
-  bootstrap: "0x727e782973DE7A3BddD8E38A6Fb0BfB69Eeed814", // SystemBootstrap su Sepolia
+  bootstrap: "0xE958Edc601A6eAa70fb4385B8d51f5C6c3b4F038", // SystemBootstrap su Sepolia
   router: "",
   factory: "",
   pollHub: "",
