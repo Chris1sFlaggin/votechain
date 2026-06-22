9d069ea7ba791b3e57b425f35b567a98f4280d72
Chris1sFlaggin <marongiuloris@gmail.com>
2026-06-22 17:10:36 +0200

    refactor

--- web/config.js
+++ web/config.js
@@ -3,6 +3,6 @@
 // di ciascuno è il governo del proprio sistema.
 const CONFIG = {
   chainId: 11155111, // Sepolia: il sito chiede a MetaMask di passare a questa rete
-  factory: "", // indirizzo GovFactory su Sepolia
-  pollHub: "", // indirizzo PollHub su Sepolia
+  factory: "0xF0FC57204F5fEB1c6942A13665259Ed648A52BFd", // indirizzo GovFactory su Sepolia
+  pollHub: "0x2eFCf24B4F90b6aCf401d572a9A601f46412Df36", // indirizzo PollHub su Sepolia
 };
